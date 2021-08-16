611016919be1388745b208bc9cd1a21b191a8cc4
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-08-16 20:51:32 +0700

    35: Outputting Dynamic Data - Working with Expressions in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 7, 16);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 297.64;
+  
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{ expenseTitle }</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
