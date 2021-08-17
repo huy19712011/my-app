a56954113f7bb07fa8528016d46f47ae424e3dfd
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-08-17 22:27:09 +0700

    51: adding form input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/newExpense/NewExpense';
 
 function App() {
 
@@ -10,10 +11,10 @@ function App() {
     { title: 'Car Insurance 3', amount: 293.67, date: new Date(2021, 7, 16) },
     { title: 'Car Insurance 4', amount: 294.67, date: new Date(2021, 7, 16) },
   ];
-
+ 
   return (
     <div className="App">
-      <h2>Let's get stated!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
