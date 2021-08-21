fb8b7aa1dd1a8ec108db4eebcbca855946ff469a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-08-21 23:03:23 +0700

    57: Two ways binding

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -13,7 +13,7 @@ const ExpenseForm = () => {
   //   enteredAmount: '',
   //   enteredDate: '',
   // });
-  
+
   const titleChangeHandler = (event) => {
 
     setEnteredTitle(event.target.value);
@@ -54,21 +54,39 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    // clear input after submit
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+
   };
 
   return <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" onChange={titleChangeHandler} />
+        <input
+          type="text"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+        />
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+        <input
+          type="number"
+          min="0.01" step="0.01"
+          value={enteredAmount}
+          onChange={amountChangeHandler}
+        />
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" min="2020-01-01" max="2025-12-31" onChange={dateChangeHandler} />
+        <input
+          type="date" min="2020-01-01" max="2025-12-31"
+          value={enteredDate}
+          onChange={dateChangeHandler}
+        />
       </div>
     </div>
     <div className="new-expense__actions">
