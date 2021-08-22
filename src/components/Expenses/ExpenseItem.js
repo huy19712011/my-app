52bec48de6b5dba6b413d1a5945e239f6af6c9e3
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-08-22 20:12:03 +0700

    60: Controlled vs UnControlled, stateless vs stateful (or dump vs smart) components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
@@ -6,24 +6,24 @@ import Card from '../ui/Card';
 
 function ExpenseItem(props) {
 
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
   
-  const clickButtonHandler = () => {
+  // const clickButtonHandler = () => {
     
-    setTitle('Updated');
-    console.log(title);
+  //   setTitle('Updated');
+  //   console.log(title);
 
-  };
+  // };
     
   return (
     <Card className="expense-item">
       <ExpenseDate date123={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{ title }</h2>
+        <h2>{ props.title }</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       {/* <button onClick={() => {console.log('Clicked')}}>Change Title</button> */}
-      <button onClick={clickButtonHandler}>Change Title</button>
+      {/* <button onClick={clickButtonHandler}>Change Title</button> */}
     </Card>
   );
 
