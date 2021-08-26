49debd430da8a28e542f3fd48577850a3f9fb229
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-08-26 17:36:12 +0700

    64: using stateful list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/newExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  { title: 'Car Insurance 1', amount: 291.67, date: new Date(2021, 7, 16) },
+  { title: 'Car Insurance 2', amount: 292.67, date: new Date(2021, 7, 16) },
+  { title: 'Car Insurance 3', amount: 293.67, date: new Date(2021, 7, 16) },
+  { title: 'Car Insurance 4', amount: 294.67, date: new Date(2021, 7, 16) },
+];
+
 function App() {
 
-  const expenses = [
-    { title: 'Car Insurance 1', amount: 291.67, date: new Date(2021, 7, 16) },
-    { title: 'Car Insurance 2', amount: 292.67, date: new Date(2021, 7, 16) },
-    { title: 'Car Insurance 3', amount: 293.67, date: new Date(2021, 7, 16) },
-    { title: 'Car Insurance 4', amount: 294.67, date: new Date(2021, 7, 16) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
 
-    console.log('In App.js');
-    console.log(expense);
+    // console.log('In App.js');
+    // console.log(expense);
+
+    //setExpenses([expense, ...expenses]); // bad
+
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
   };
  
   return (
